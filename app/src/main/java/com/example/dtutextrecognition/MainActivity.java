6c61f9a54d05befe44644301b9954dd0f41d1317
Priyesh Chaurasia <39457163+priyeshchaurasia@users.noreply.github.com>
2020-07-01 15:21:45 +0530

    CameraPreview

--- app/src/main/java/com/example/dtutextrecognition/MainActivity.java
+++ app/src/main/java/com/example/dtutextrecognition/MainActivity.java
@@ -1,15 +1,204 @@
 package com.example.dtutextrecognition;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener, View.OnClickListener {
+    private TextureView textureView;
+    private Button search, flash;
+    CameraCharacteristics cameraCharacteristics;
+    CameraDevice cameraDevice;
+    private String cameraID;
+    CameraCaptureSession cameraCaptureSession;
+    Size dimension;
+    CameraManager cameraManager;
+    SurfaceTexture texture;
+    int CAMERA_PERMISSION_CODE = 1;
+
+    CaptureRequest.Builder captureBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textureView = (TextureView) findViewById(R.id.photoTexture);
+        textureView.setSurfaceTextureListener(this);
+        search = findViewById(R.id.search);
+        search.setOnClickListener(this);
+        flash = findViewById(R.id.flash);
+        flash.setOnClickListener(this);
+
+
     }
-    
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Log.d("TAGGG", "onStart");
+        requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                try {
+                    handleCamera();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                Toast.makeText(this, "You Cannot Use the App", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        texture = surface;
+
+        try {
+            handleCamera();
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void handleCamera() throws CameraAccessException {
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        cameraID = cameraManager.getCameraIdList()[0];
+        cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraID);
+        CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+            @Override
+            public void onOpened(@NonNull CameraDevice camera) {
+                cameraDevice = camera;
+                try {
+                    cameraPreview();
+                } catch (Exception e) {
+
+                }
+
+            }
+
+            @Override
+            public void onDisconnected(@NonNull CameraDevice camera) {
+                camera.close();
+                cameraDevice = null;
+            }
+
+            @Override
+            public void onError(@NonNull CameraDevice camera, int error) {
+                camera.close();
+                cameraDevice = null;
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        cameraManager.openCamera(cameraID, stateCallback, null);
+
+
+
+
+    }
+
+    private void cameraPreview() throws CameraAccessException {
+        StreamConfigurationMap map =cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+        dimension = map.getOutputSizes(SurfaceTexture.class)[0];
+        texture.setDefaultBufferSize(dimension.getWidth(),dimension.getHeight());
+        Surface surface = new Surface(texture);
+        captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+        captureBuilder.addTarget(surface);
+        cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+            @Override
+            public void onConfigured(@NonNull CameraCaptureSession session) {
+                if(cameraDevice !=null){
+                    cameraCaptureSession = session;
+                    try {
+                        captureBuilder.set(CaptureRequest.CONTROL_AF_MODE, CameraMetadata.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                        cameraCaptureSession.setRepeatingRequest(captureBuilder.build(),null,null);
+
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+            }
+        },null);
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
